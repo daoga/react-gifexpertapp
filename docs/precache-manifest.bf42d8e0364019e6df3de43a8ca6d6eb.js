862d4fb3bebdcb0bd7a1e9db02694f5e8483eb91
Diego <die.moralesh@gmail.com>
2020-07-21 22:37:17 -0400

    doc agregado

---
+++ docs/precache-manifest.bf42d8e0364019e6df3de43a8ca6d6eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41e79791ff9e0a8c2d399b7a68475280",
+    "url": "/index.html"
+  },
+  {
+    "revision": "111ba6b72486ba26c0d8",
+    "url": "/static/css/main.1deea9f7.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "111ba6b72486ba26c0d8",
+    "url": "/static/js/main.9b48d08d.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
